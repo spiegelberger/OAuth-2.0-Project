4082148e72e3fe03eb01f6db98ef1e3060ba0f0b
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-06-30 07:49:26 +0200

    Scope based access control implemented.

--- ResourceServer/src/main/java/com/example/resourceserver/security/WebSecurity.java
+++ ResourceServer/src/main/java/com/example/resourceserver/security/WebSecurity.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter{
@@ -11,13 +12,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
+		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+		converter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+		
 		http
 			.authorizeRequests()
-				.antMatchers(HttpMethod.GET, "/users/status/check").hasAuthority("SCOPE_profile")
+				.antMatchers(HttpMethod.GET, "/users/status/check")
+					//.hasAuthority("SCOPE_profile")
+					//.hasAuthority("ROLE_developer")
+					.hasRole("developer")
 				.anyRequest().authenticated()
 			.and()
 			.oauth2ResourceServer()
-			.jwt();
+			.jwt()
+			.jwtAuthenticationConverter(converter);
 	}
 	
 }
