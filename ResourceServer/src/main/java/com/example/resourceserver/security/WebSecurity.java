92e5632e1f5878f34661d723cd54a11876e3b754
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-06-30 05:40:09 +0200

    WebSecurity class added.

---
+++ ResourceServer/src/main/java/com/example/resourceserver/security/WebSecurity.java
@@ -0,0 +1,23 @@
+package com.example.resourceserver.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/users/status/check").hasAuthority("SCOPE_profile")
+				.anyRequest().authenticated()
+			.and()
+			.oauth2ResourceServer()
+			.jwt();
+	}
+	
+}
