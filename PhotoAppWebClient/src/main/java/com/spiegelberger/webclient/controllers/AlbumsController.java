a35ff7a452467176e2e5bb722d71581eebc9a570
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-07-02 12:39:43 +0200

    Reading ID Token and Jwt Access Token.

--- PhotoAppWebClient/src/main/java/com/spiegelberger/webclient/controllers/AlbumsController.java
+++ PhotoAppWebClient/src/main/java/com/spiegelberger/webclient/controllers/AlbumsController.java
@@ -3,8 +3,13 @@ package com.spiegelberger.webclient.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +20,26 @@ import com.spiegelberger.webclient.response.AlbumRest;
 
 @Controller
 public class AlbumsController {
+	
+	@Autowired
+	OAuth2AuthorizedClientService oauth2ClientService;
 
 	
 	@GetMapping("/albums")
 	public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+		
+		// Read the JWT Access Token
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		// cast it
+		OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+		
+		OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(
+				oauthToken.getAuthorizedClientRegistrationId(), oauthToken.getName() );
+		
+		String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+		
+//		System.out.println("JWT ACCESS TOKEN: " + jwtAccessToken);
 		
 		// Principal Informations
 //		System.out.println("Principal: " + principal);
