22bb67d8982f463f47765d2f0cd1e1598aa6e873
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-07-03 09:46:05 +0200

    getAlbumsUsingWebClient added.

--- PhotoAppWebClient/src/main/java/com/spiegelberger/webclient/controllers/AlbumsController.java
+++ PhotoAppWebClient/src/main/java/com/spiegelberger/webclient/controllers/AlbumsController.java
@@ -1,6 +1,5 @@
 package com.spiegelberger.webclient.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.spiegelberger.webclient.response.AlbumRest;
 
@@ -33,6 +33,8 @@ public class AlbumsController {
 	@Autowired
 	RestTemplate restTemplate; 
 
+	@Autowired
+	WebClient webClient;
 	
 	@GetMapping("/albums")
 	public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
@@ -85,6 +87,21 @@ public class AlbumsController {
 		
 		model.addAttribute("albums", returnValue);
 */		
+		return "albums";
+	}
+	
+	@GetMapping("/albumsWebClient")
+	public String getAlbumsUsingWebClient(Model model, 
+			@AuthenticationPrincipal OidcUser principal) {
+		
+		String url = "http://localhost:8082/albums";
+		List<AlbumRest>albums = webClient.get()
+					.uri(url)
+					.retrieve()
+					.bodyToMono(new ParameterizedTypeReference<List<AlbumRest>>() {} )
+					.block();
+		
+		model.addAttribute("albums", albums);
 		return "albums";
 	}
 }
