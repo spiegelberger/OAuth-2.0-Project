ef28847ab548ffa7c5d246b5190304f80469d14d
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2021-07-04 09:33:35 +0200

    SocialLoginWebClient added.

---
+++ SocialLoginWebClient/src/main/java/com/spiegelberger/socialloginwebclient/controllers/IndexPageController.java
@@ -0,0 +1,18 @@
+package com.spiegelberger.socialloginwebclient.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexPageController {
+	
+	
+	@GetMapping("/")
+	public String displayIndexPage(Model model) {
+		
+		return "index";
+	}
+
+}
